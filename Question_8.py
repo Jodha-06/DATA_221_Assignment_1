228d176369c94c1d301bf018e82f51c60403c6d4
Jodha-06 <Jodhveerd06@gmail.com>
2026-01-28 20:40:27 -0700

    Add comments to Question_8.py for improved readability and understanding

--- Question_8.py
+++ Question_8.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
+#Dictionary containing the data for the Data Frame
 Data_for_the_Data_Frame = {
     "A":[1,2,2,1],
     "B":[3.1,4.2,1.5,6.3],
     "C":[800,150,400,210]
 }
 
+# Create the Data Frame using values from the Dictionary
 Final_Data_Frame = pd.DataFrame(Data_for_the_Data_Frame)
 
+# Create a new column "D" by multiplying columns "A" and "B"
 Final_Data_Frame["D"] = Final_Data_Frame["A"]*Final_Data_Frame["B"]
 
+#Print the final Data Frame
 print(Final_Data_Frame)
