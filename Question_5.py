446387b9e364c09d089e5fb2c1729a4da3f9af41
Jodha-06 <Jodhveerd06@gmail.com>
2026-01-28 20:33:15 -0700

    Add comments to Question_5.py for improved readability and understanding

--- Question_5.py
+++ Question_5.py
@@ -2,19 +2,24 @@ import math
 
 def circleAreaCoverage(Radius_of_Circle_1, Radius_of_Circle_2):
 
+    # Check that both inputs are integers
     if type(Radius_of_Circle_1) != int or  type(Radius_of_Circle_2) != int:
         return "Both radii must be positive integers"
 
+    # Check that both integers are positive
     if Radius_of_Circle_1 <= 0 or Radius_of_Circle_2 <= 0:
         return "Both radii must be positive integers."
 
+    # Calculate the area of each circle
     Area_of_Circle_1 = math.pi * math.pow(Radius_of_Circle_1, 2)
     Area_of_Circle_2 = math.pi * math.pow(Radius_of_Circle_2, 2)
 
+    # If circle 1 is larger, compute the percentage that is covered by circle 2
     if Area_of_Circle_1 > Area_of_Circle_2:
         Percentage_of_Circle_1_Covered = round((Area_of_Circle_2 / Area_of_Circle_1) * 100,2)
         return Percentage_of_Circle_1_Covered
 
+    ## If circle 2 is larger, compute the percentage that is covered by circle 1
     elif Area_of_Circle_2 > Area_of_Circle_1:
         Percentage_of_Circle_2_Covered = round((Area_of_Circle_1 / Area_of_Circle_2) * 100,2)
         return Percentage_of_Circle_2_Covered
@@ -23,10 +28,10 @@ def circleAreaCoverage(Radius_of_Circle_1, Radius_of_Circle_2):
         return 100
 
 
-#Test Cases
+#Test cases
 print(circleAreaCoverage(3, 5))
 print(circleAreaCoverage(5, -2))
-print(circleAreaCoverage(5, 7))
+print(circleAreaCoverage(7, 5))
 print(circleAreaCoverage(-6, 4))
 print(circleAreaCoverage(5, 3.0))
 print(circleAreaCoverage(4, 4))
