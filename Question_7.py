9ca0d6dd843ff63f38217f9c47604bad1b381439
Jodha-06 <Jodhveerd06@gmail.com>
2026-01-28 20:39:58 -0700

    Add comments to Question_7.py for improved readability and understanding

--- Question_7.py
+++ Question_7.py
@@ -1,19 +1,23 @@
 def Convert_Seconds_Since_Midnight_Into_Readable_Time(Seconds_Since_Midnight):
-    # 24 x 60 x 60 = 86400 Seconds. The input must be below 86400 seconds so the code will calculate the value of seconds since midnight for the same day
+    # Check for valid input range ( There are 86400 seconds in a day)
     if Seconds_Since_Midnight < 0 or Seconds_Since_Midnight >86400:
         return "Invalid Input. Input must be between 0 and 86399"
 
+    # Calculate hours
     Hour_Value_in_Result = Seconds_Since_Midnight // 3600
     Remaining_Seconds_Value = Seconds_Since_Midnight % 3600
 
+    # Calculate minutes and seconds
     Minute_Value_in_Result = Remaining_Seconds_Value // 60
     Final_Seconds_Value = Remaining_Seconds_Value % 60
 
+    #Determine AM or PM
     if Hour_Value_in_Result >= 12:
         Period_of_the_Day = 'PM'
     else:
         Period_of_the_Day = 'AM'
 
+    #Convert final time to 12 hour format
     if Hour_Value_in_Result == 0:
         Hour_Value_in_Result = 12
     elif Hour_Value_in_Result > 12:
