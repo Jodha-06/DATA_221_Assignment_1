db38e32f3a9d9199de10f1f32cb431c652423891
Jodha-06 <Jodhveerd06@gmail.com>
2026-01-28 20:19:39 -0700

    Add comments to Question_2.py for improved readability and understanding

--- Question_2.py
+++ Question_2.py
@@ -1,24 +1,33 @@
 def String_Analysis_Dictionary(List_of_Strings):
+
+    #Create the final dictionary
     Final_Analysis_Dictionary = {}
 
+    # Loop through each string that is in the list
     for Current_String in List_of_Strings:
+        # Find the length of a selected string
         Current_String_Length = len(Current_String)
 
+        #Determine if the length of the string is even or odd
         if Current_String_Length % 2 == 0:
             Parity_Value = "even"
         else:
             Parity_Value = "odd"
 
+        # Store results in the final dictionary
         Final_Analysis_Dictionary[Current_String]={
             "length": Current_String_Length,
             "parity": Parity_Value
         }
 
+    # Return the final dictionary
     return Final_Analysis_Dictionary
 
+# Sample input list
 Sample_List_of_Inputs= ["data", "science", "university", "code"]
 Outputs_Dictionary = String_Analysis_Dictionary(Sample_List_of_Inputs)
 
+# Print dictionary in the format outlined in the Assignment description
 print("{")
 
 for Current_Key in Outputs_Dictionary:
