5896775d536e312cdd857d242e58741b0eba1a6c
Jodha-06 <Jodhveerd06@gmail.com>
2026-01-28 20:35:26 -0700

    Add comments to Question_6.py for improved readability and understanding

--- Question_6.py
+++ Question_6.py
@@ -1,15 +1,21 @@
 def Distribution_Analysis_of_Numbers(List_of_Numbers):
+    # Dictionary to store final results
     Dictionary_of_Numbers = {}
+    # Total number of elements in a given list
     Total_Elements_in_List = len(List_of_Numbers)
 
+    # List to store unique values
     List_of_Unique_Values = []
 
+    # Find unique values from the list
     for Individual_Numbers in List_of_Numbers:
         if Individual_Numbers not in List_of_Unique_Values:
             List_of_Unique_Values.append(Individual_Numbers)
 
+    #Sort the unique values
     List_of_Unique_Values.sort()
 
+    #Calculate percentage for each unique value
     for Key_Values in List_of_Unique_Values:
         Less_Than_or_Equal_to = 0
 
@@ -22,7 +28,7 @@ def Distribution_Analysis_of_Numbers(List_of_Numbers):
 
     return Dictionary_of_Numbers
 
-
+# Test Case
 List_of_Numbers = [3,1,2,3,4,2]
 Final_Result = Distribution_Analysis_of_Numbers(List_of_Numbers)
 print(Final_Result)
