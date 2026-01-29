eab88358614fb0b204d7f1881cf594e741ebbaf1
Jodha-06 <Jodhveerd06@gmail.com>
2026-01-28 20:14:09 -0700

    Add comments to Question_1.py for improved readability and understanding

--- Question_1.py
+++ Question_1.py
@@ -1,13 +1,16 @@
+# Store the initial threshold value
 Product_Threshold_Value = 100
-
+# Initialize the Current Product Value to 1
 Accumulated_Product_Value = 1
+# Start multiplying from 1
 Current_Multiplier = 1
 
-
+#Continue multiplying until the product becomes larger than the threshold value
 while Accumulated_Product_Value <= Product_Threshold_Value:
     Current_Multiplier += 1
     Accumulated_Product_Value *= Current_Multiplier
 
-
+#print the final product
 print(f"The final product is: {Accumulated_Product_Value}")
+#print the integer that caused the product to exceed the threshold
 print(f"The integer that cause the product to exceed the given threshold value is: {Current_Multiplier}")
